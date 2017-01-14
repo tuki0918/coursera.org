1e183988596bf2c6625d08711f4d5be28682a2aa
tuki0918 <tuki0918@gmail.com>
2017-01-14 13:13:42 +0900

    week5 task
    http://www.cnblogs.com/CheeseZH/p/4608129.html

--- machine-learning/week5/machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning/week5/machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,23 +62,47 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Part 1
+Y = eye(num_labels)(y, :);
+a1 = [ones(m, 1), X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m, 1), a2];
+z3 = a2 * Theta2';
+h = a3 = sigmoid(z3);
 
+cost = -Y .* log(h) - (1 - Y) .* log(1 - h);
+J = (1/m) * sum(cost(:));
 
+Theta1Filtered = Theta1(:, 2:end);
+Theta2Filtered = Theta2(:, 2:end);
+reg = (lambda/(2*m)) * (sumsq(Theta1Filtered(:)) + sumsq(Theta2Filtered(:)));
+J = J + reg;
 
+% Part 2
+Delta1 = 0;
+Delta2 = 0;
 
+for t = 1:m,
+  a1 = [1 X(t,:)];
+  z2 = a1 * Theta1';
+  a2 = [1 sigmoid(z2)];
+  z3 = a2 * Theta2';
+  h = a3 = sigmoid(z3);
 
+  d3 = a3 - Y(t, :);
+  d2 = (d3 * Theta2Filtered) .* sigmoidGradient(z2);
 
+  Delta1 = Delta1 + (d2' * a1);
+  Delta2 = Delta2 + (d3' * a2);
+end;
 
+Theta1_grad = (1/m) * Delta1;
+Theta2_grad = (1/m) * Delta2;
 
-
-
-
-
-
-
-
-
-
+% Part 3
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + ((lambda/m) * Theta1Filtered);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + ((lambda/m) * Theta2Filtered);
 
 % -------------------------------------------------------------
 
