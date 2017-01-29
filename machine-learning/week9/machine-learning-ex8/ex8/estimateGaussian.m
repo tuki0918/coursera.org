518d9dbb1a1f9cbfa7cd672509a9da8767a9d594
tuki0918 <tuki0918@gmail.com>
2017-01-29 17:08:08 +0900

    week9 task 2

--- machine-learning/week9/machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning/week9/machine-learning-ex8/ex8/estimateGaussian.m
@@ -21,10 +21,14 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-mu = (1/m) * sum(X);
-for i=1:n,
-    sigma2(i) = (1/m) * sum(((X(:, i) - mu(i)) .^ 2));
-end
+% mu = (1/m) * sum(X);
+% for i=1:n,
+%     sigma2(i) = (1/m) * sum(((X(:, i) - mu(i)) .^ 2));
+% end
+
+mu = mean(X)';
+X2 = (X - ones(m, 1) * mu') .^ 2;
+sigma2 = mean(X2);
 
 % =============================================================
 
